130597b3e9cb45c71ce4b0998eef3a7ebef6dbc3
sidney <516202795@qq.com>
2021-03-29 02:13:38 +0800

    remove reference of appdelegate

--- Example/Dplayer/AppDelegate.swift
+++ Example/Dplayer/AppDelegate.swift
@@ -8,20 +8,10 @@
 
 import UIKit
 
-let appDelegate = UIApplication.shared.delegate as! AppDelegate
-
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-    func hideStatusBar() {
-        UIApplication.shared.statusBarUIView?.isHidden = true
-    }
-
-    func showStatusBar() {
-        UIApplication.shared.statusBarUIView?.isHidden = false
-    }
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -52,28 +42,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-
-extension UIApplication {
-    var statusBarUIView: UIView? {
-        if #available(iOS 13.0, *) {
-            let tag = 3848245
-            let keyWindow: UIWindow? = appDelegate.window
-            if let statusBar = keyWindow?.viewWithTag(tag) {
-                return statusBar
-            } else {
-                let height = keyWindow?.windowScene?.statusBarManager?.statusBarFrame ?? .zero
-                let statusBarView = UIView(frame: height)
-                statusBarView.tag = tag
-                statusBarView.layer.zPosition = 999999
-                keyWindow?.addSubview(statusBarView)
-                return statusBarView
-            }
-        } else {
-            if responds(to: Selector(("statusBar"))) {
-                return value(forKey: "statusBar") as? UIView
-            }
-        }
-        return nil
-    }
-}
-
